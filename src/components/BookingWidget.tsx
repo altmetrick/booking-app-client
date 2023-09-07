3dcdc07939bf58a754ace5c857ac35fd44b71245
Altmertrick <altmertricks@gmail.com>
2023-09-07 19:12:10 +0300

    added DateInterPicker for check in/out bookinng interval

--- src/components/BookingWidget.tsx
+++ src/components/BookingWidget.tsx
@@ -1,13 +1,117 @@
 import { useState } from 'react';
 import { PlaceT } from '../types';
-import { differenceInCalendarDays } from 'date-fns';
+import { addDays, addMonths, differenceInCalendarDays, isBefore, isWithinInterval } from 'date-fns';
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 type PropsT = { place: PlaceT };
 
+const bookedRanges = [
+  { start: new Date('2023-09-06'), end: new Date('2023-09-09') },
+  { start: new Date('2023-09-13'), end: new Date('2023-09-15') },
+  { start: new Date('2023-09-20'), end: new Date('2023-09-25') },
+];
+
+type DateIntervalPikerPropsT = {
+  startId: string;
+  endId: string;
+  startLabel: string;
+  endLabel: string;
+  startDate: Date | null;
+  endDate: Date | null;
+  setStartDate: (date: Date | null) => void;
+  setEndDate: (date: Date | null) => void;
+  excludedDateIntervals?: { start: Date; end: Date }[];
+};
+
+const DateIntervalPiker: React.FunctionComponent<DateIntervalPikerPropsT> = ({
+  startId,
+  startLabel,
+  startDate,
+  setStartDate,
+  endId,
+  endLabel,
+  endDate,
+  setEndDate,
+  excludedDateIntervals,
+}) => {
+  const getEndOfConsecutiveRange = (
+    currStart: Date,
+    excludedRanges: { start: Date; end: Date }[]
+  ) => {
+    for (let i = 0; i < excludedRanges.length; i++) {
+      const excludedRangeStart = excludedRanges[i].start;
+
+      if (startDate?.getUTCDay() === excludedRangeStart.getUTCDay()) {
+        return addDays(excludedRangeStart, 1);
+      }
+
+      if (isBefore(currStart, excludedRangeStart)) return excludedRangeStart;
+    }
+    return null;
+  };
+
+  let currConsecutiveRangeEnd: null | Date = null;
+  if (startDate && excludedDateIntervals) {
+    currConsecutiveRangeEnd = getEndOfConsecutiveRange(startDate, excludedDateIntervals);
+  }
+
+  const isInBookedRange = (date: Date) => {
+    if (currConsecutiveRangeEnd === null) return true;
+    return isWithinInterval(date, { start: startDate || new Date(), end: currConsecutiveRangeEnd });
+  };
+
+  const handleSetStartDate = (date: Date) => {
+    setStartDate(date);
+    setEndDate(date);
+  };
+
+  return (
+    <>
+      <div className="cursor-pointer">
+        <label htmlFor={startId} className="cursor-pointer uppercase text-sm text-gray-700">
+          {startLabel}
+        </label>
+        <DatePicker
+          id={startId}
+          showIcon
+          minDate={new Date()}
+          maxDate={addMonths(new Date(), 3)}
+          selected={startDate}
+          onChange={(date) => handleSetStartDate(date || new Date())}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          excludeDateIntervals={bookedRanges}
+        />
+      </div>
+      {/* Checkout */}
+      <div className="cursor-pointer">
+        <label htmlFor={endId} className="cursor-pointer uppercase text-sm text-gray-700">
+          {endLabel}
+        </label>
+        <DatePicker
+          showIcon={true}
+          minDate={startDate}
+          maxDate={addMonths(new Date(), 3)}
+          startDate={startDate}
+          endDate={endDate}
+          selected={endDate}
+          onChange={(date) => setEndDate(date || new Date())}
+          selectsEnd
+          excludeDateIntervals={excludedDateIntervals || []}
+          filterDate={isInBookedRange}
+        />
+      </div>
+    </>
+  );
+};
+
 export const BookingWidget: React.FunctionComponent<PropsT> = ({ place }) => {
-  const [checkIn, setCheckIn] = useState('');
-  const [checkOut, setCheckOut] = useState('');
   const [numOfGuests, setNumOfGuests] = useState(1);
+  const [checkIn, setCheckIn] = useState<Date | null>(null);
+  const [checkOut, setCheckOut] = useState<Date | null>(null);
 
   return (
     <div className="p-4 border border-gray-300 rounded-2xl shadow-md sticky top-10 h-fit">
@@ -15,33 +119,20 @@ export const BookingWidget: React.FunctionComponent<PropsT> = ({ place }) => {
         <span className="font-semibold text-lg">${place?.price}</span>{' '}
         <span className="text-md text-gray-700 font-light">night</span>
       </div>
-      {/* Check-In */}
-      <div className="my-3 p-3 min-w-[13rem] border border-gray-300 rounded-xl grid grid-cols-2 gap-4">
-        <div className="cursor-pointer">
-          <label htmlFor="checkIn" className="cursor-pointer uppercase text-sm text-gray-700">
-            Check-In
-          </label>
-          <input
-            id="checkIn"
-            type="date"
-            className="cursor-pointer w-full text-sm"
-            value={checkIn}
-            onChange={(e) => setCheckIn(e.target.value)}
-          />
-        </div>
-        {/* Checkout */}
-        <div className="cursor-pointer">
-          <label htmlFor="checkOut" className="cursor-pointer uppercase text-sm text-gray-700">
-            Check Out
-          </label>
-          <input
-            id="checkOut"
-            type="date"
-            className="cursor-pointer w-full text-sm"
-            value={checkOut}
-            onChange={(e) => setCheckOut(e.target.value)}
-          />
-        </div>
+
+      <div className="my-3 p-3 min-w-[13rem] border border-gray-300 rounded-xl">
+        <DateIntervalPiker
+          startDate={checkIn}
+          setStartDate={setCheckIn}
+          startId={'checkIn'}
+          startLabel="Check-In"
+          endDate={checkOut}
+          setEndDate={setCheckOut}
+          endId={'checkOut'}
+          endLabel="Checkout"
+          excludedDateIntervals={bookedRanges}
+        />
+
         {/* Num Of Guests */}
         <div className="col-span-2">
           <label htmlFor="numOfGuests" className="uppercase text-sm text-gray-700">
